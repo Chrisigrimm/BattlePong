5062fe9ddbfae959165935e2498b4bd6f43b976d
Keyjin <keyjin@hotmail.de>
2014-04-28 02:40:36 +0200

    Clear Files, Fixed Bot, Change PlayerPos

---
+++ Assets/Code/Scripts/Gaming/CountDown.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CountDown : MonoBehaviour {
+	public float Timer = 3;
+	float saveTimer = 3;
+	bool Visible = true;
+	public GUISkin theSkin;
+	// Update is called once per frame
+	void Start(){
+		saveTimer = Timer;
+	}
+
+	void cDown(){
+		Timer = saveTimer;
+		Visible = true;
+		InvokeRepeating("Delay", 1, 1);
+	}
+
+	void Delay(){
+		if(Timer !=0 ){
+			Timer--;
+		}else{
+			GameObject.Find("Ball").SendMessage ("GoBall");
+			Timer=saveTimer;
+			Visible = false;
+			CancelInvoke("Delay");
+		}
+	}
+	void Update () {
+	}
+
+	void OnGUI(){
+		GUI.skin = theSkin;
+		if(Timer > 0 && Visible){
+			GUI.Label( new Rect(  Screen.width/2 -25 , 25 , 100 ,200 ),"" + Timer );
+		}
+		if(Timer == 0 && Visible){
+			GUI.Label( new Rect( Screen.width/2 -75 , 25 , 200 ,200 ),"Go!" );
+		}
+	}
+}
---
+++ Assets/Code/Scripts/Gaming/GameSetup.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+   //////////////
+  ////G-Tec/////
+ ////Keyjin////
+//////////////
+
+public class GameSetup : MonoBehaviour {
+	
+	public Camera mainCam;
+	public BoxCollider2D leftWall, rightWall;
+	//public Transform  ButtomWall, TopWall, Player01, Player02;
+
+	public static GameObject pausedPanel;
+
+	// Use this for initialization
+	void Start () {
+		/*ButtomWall.transform.position = new Vector3 (0f,mainCam.ScreenToWorldPoint ( new Vector3 (0f, Screen.height , 0f)).y - ButtomWall.GetComponent<BoxCollider2D>().size.y/2, 1f);
+		TopWall.transform.position = new Vector3 (0f,mainCam.ScreenToWorldPoint ( new Vector3 (0f, 0f, 0f)).y + TopWall.GetComponent<BoxCollider2D>().size.y/2, 1f);
+		*/
+		/*leftWall.size = new Vector2 (1f, mainCam.ScreenToWorldPoint ( new Vector3 (0f , Screen.height*2f, 0f)).y);
+		leftWall.center = new Vector2 (mainCam.ScreenToWorldPoint ( new Vector3 ( 0f , 0f , 0f)).x - 0.5f,0f);
+		
+		rightWall.size = new Vector2 (1f, mainCam.ScreenToWorldPoint ( new Vector3 (0f , Screen.height * 2f, 0f)).y);
+		rightWall.center = new Vector2 (mainCam.ScreenToWorldPoint ( new Vector3 (  Screen.width ,0f, 0f)).x+ 0.5f, 0f);*/
+		
+		pausedPanel = GameObject.Find("Window - Paused");
+		NGUITools.SetActive(pausedPanel,false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
